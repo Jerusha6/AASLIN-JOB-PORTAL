eccf5f7d8d00e7a1f13df00d1b4086caa2686e68
Aditya <147597169+aditya6305@users.noreply.github.com>
2025-09-02 12:19:38 +0530

    merged

--- src/main/java/com/aaslin/jobportal/UserData.java
+++ src/main/java/com/aaslin/jobportal/UserData.java
@@ -1,17 +1,51 @@
 package com.aaslin.jobportal;
 
+import java.util.Arrays;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="users_data")
 public class UserData {
     @Id
     private String name;
+    @Lob
+    @Column(name = "resume", columnDefinition = "LONGBLOB")  // <-- Fix here
     private byte[] resume;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public byte[] getResume() {
+		return resume;
+	}
+	public void setResume(byte[] resume) {
+		this.resume = resume;
+	}
+	public UserData() {
+		super();
+	}
+	public UserData(String name, byte[] resume) {
+		super();
+		this.name = name;
+		this.resume = resume;
+	}
+	@Override
+	public String toString() {
+		return "UserData [name=" + name + ", resume=" + Arrays.toString(resume) + "]";
+	}
+	
+	
 }
